1357eeb5d7c6b25c1108b39282365fd347a0ecb3
SpicyRamen <94422507+itotoostressed@users.noreply.github.com>
2025-02-10 14:49:49 -0500

    Update Main.java

--- Diet tracker/Diet tracker/src/Main.java
+++ Diet tracker/Diet tracker/src/Main.java
@@ -1,11 +1,11 @@
-import org.apache.commons.math3.optim.linear.*;
 public class Main {
     public static void main(String[] args) {
         Menu mainMenu = new Menu();
         while (!mainMenu.getLogin()) {
             mainMenu.setVisible(true);
         }
-            DietGUI frame = new DietGUI();
-            frame.setVisible(true);
+        mainMenu.setVisible(false);
+        DietGUI frame = new DietGUI();
+        frame.setVisible(true);
     }
 }
